d1f2f9f86e3ce663fd37bb7fe2c356a2f93b1a3b
Luca Pellegrini <nepero@gmail.com>
2021-11-15 10:57:38 +0100

    Added credentials file sample

---
+++ credentials.py
@@ -0,0 +1,3 @@
+credentials = {'bearer': 'ABCDEFGHILMNOPQRSTUVZ012345678901234567890123456ZWIxNzEyMTctZjU1Ni00OTJkLWFlZjUtNzVmOTBhZDg3NDJmNzYyMTA1OWItYjQ4_PF84_c4096449-70ba-46fb-a2d8-d12967412d6c',
+               'filename': '/Users/folder1/WxC_API/agent_list.xlsx'
+              }
