c895998e45f68bc9e5c7069cd4e2f8fc1fad43b1
Luca Pellegrini <nepero@gmail.com>
2021-11-15 12:12:21 +0100

    updated credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-credentials = {'bearer': 'ABCDEFGHILMNOPQRSTUVZ012345678901234567890123456ZWIxNzEyMTctZjU1Ni00OTJkLWFlZjUtNzVmOTBhZDg3NDJmNzYyMTA1OWItYjQ4_PF84_c4096449-70ba-46fb-a2d8-d12967412d6c',
-               'filename': '/Users/folder1/WxC_API/agent_list.xlsx'
+credentials = {'bearer': 'ABCDEFGHILMNOPQRSTUVZ012345678901234567890123456ZWIxNzEyMTctZjU1Ni00OTJkLWFlZjUtNzVmOTBhZDg3NDJmNzYyMTA1OWItYjQ4_PF84_c4096449-70ba-46fb-a2d8-d12967412d6c', #admin bearer token from developer.webex.com webpage
+               'filename': '/Users/folder1/WxC_API/sample_provisioning_file.xlsx' #full path and name of the provisioning file
               }
